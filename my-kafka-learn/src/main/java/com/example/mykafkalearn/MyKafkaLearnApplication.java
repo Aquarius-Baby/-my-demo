84942efd10455f6dce75f559c465700171fe98cb
cmy <chenmy@weipaitang.com>
2020-10-26 10:02:43 +0800

    modify: kafka1

---
+++ my-kafka-learn/src/main/java/com/example/mykafkalearn/MyKafkaLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.mykafkalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyKafkaLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyKafkaLearnApplication.class, args);
+    }
+
+}
---
+++ my-kafka-learn/src/test/java/com/example/mykafkalearn/MyKafkaLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mykafkalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyKafkaLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
